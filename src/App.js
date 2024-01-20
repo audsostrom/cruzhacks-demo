4060efb3c55bf9d48cb383c30a1dab01dddcf14c
audsostrom <audreyostrom64@gmail.com>
2024-01-20 12:32:59 -0800

    initial presentation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import cruzhacks from './assets/cruzhacks.svg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <div className="top-bar">
+        <div className="title-text">CRUZHACKS PRESENTATION</div>
+        <img className="cruzhacks-logo" src={cruzhacks}/>
+      </div>
+      <div className="list-wrapper">
+        <div className="subtitle">Tips for Success at Hackathons</div>
+        <ol type="1">
+          <li>Drink lots of water and get some rest</li>
+          <li>Find a great team to collaborate with</li>
+          <li>Never stop coding!</li>
+        </ol>
+      </div>
     </div>
   );
 }
