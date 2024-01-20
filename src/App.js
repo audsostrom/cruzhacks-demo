5ce5955a7602360dbef524136dc5c422bd76bd3f
audsostrom <audreyostrom64@gmail.com>
2024-01-20 13:22:56 -0800

    starting files

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import cruzhacks from './assets/cruzhacks.svg';
 function App() {
   return (
     <div className="content">
-      <div className="top-bar">
-        <div className="title-text">CRUZHACKS PRESENTATION</div>
-        <img className="cruzhacks-logo" src={cruzhacks}/>
-      </div>
-      <div className="list-wrapper">
-        <div className="subtitle">Tips for Success at Hackathons</div>
-        <ol type="1">
-          <li>Drink lots of water and get some rest</li>
-          <li>Find a great team to collaborate with</li>
-          <li>Never stop coding!</li>
-        </ol>
-      </div>
+      start
     </div>
   );
 }
